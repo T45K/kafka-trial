197707348aa2b852a5c668f304f2012f07a4dc17
T45K <tasktas9@gmail.com>
2024-06-01 13:42:28 +0900

    reformat

--- src/main.kt
+++ src/main.kt
@@ -12,43 +12,43 @@ import kotlin.time.toJavaDuration
 private const val KAFKA_BOOTSTRAP_SERVER = "localhost:9092"
 
 fun main() {
-  val producerProps = mapOf(
-    "bootstrap.servers" to KAFKA_BOOTSTRAP_SERVER,
-    "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
-    "value.serializer" to "org.apache.kafka.common.serialization.ByteArraySerializer",
-    "security.protocol" to "PLAINTEXT"
-  )
+    val producerProps = mapOf(
+        "bootstrap.servers" to KAFKA_BOOTSTRAP_SERVER,
+        "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
+        "value.serializer" to "org.apache.kafka.common.serialization.ByteArraySerializer",
+        "security.protocol" to "PLAINTEXT"
+    )
 
-  KafkaProducer<String, ByteArray>(producerProps).use {
-    it.send(ProducerRecord("test", "1", "Hello, world!".encodeToByteArray()))
-  }
-
-  val consumerProps = mapOf(
-    "bootstrap.servers" to KAFKA_BOOTSTRAP_SERVER,
-    "auto.offset.reset" to "earliest",
-    "key.deserializer" to "org.apache.kafka.common.serialization.StringDeserializer",
-    "value.deserializer" to "org.apache.kafka.common.serialization.ByteArrayDeserializer",
-    "group.id" to "someGroup",
-    "security.protocol" to "PLAINTEXT"
-  )
-
-  KafkaConsumer<String, ByteArray>(consumerProps).use { consumer ->
-    consumer.subscribe(listOf("test"))
-    val message = repeatUntilSome {
-      consumer.poll(400.milliseconds.toJavaDuration())
-        .map { String(it.value()) }
-        .firstOrNull()
+    KafkaProducer<String, ByteArray>(producerProps).use {
+        it.send(ProducerRecord("test", "1", "Hello, world!".encodeToByteArray()))
     }
-    println(message)
-  }
+
+    val consumerProps = mapOf(
+        "bootstrap.servers" to KAFKA_BOOTSTRAP_SERVER,
+        "auto.offset.reset" to "earliest",
+        "key.deserializer" to "org.apache.kafka.common.serialization.StringDeserializer",
+        "value.deserializer" to "org.apache.kafka.common.serialization.ByteArrayDeserializer",
+        "group.id" to "someGroup",
+        "security.protocol" to "PLAINTEXT"
+    )
+
+    KafkaConsumer<String, ByteArray>(consumerProps).use { consumer ->
+        consumer.subscribe(listOf("test"))
+        val message = repeatUntilSome {
+            consumer.poll(400.milliseconds.toJavaDuration())
+                .map { String(it.value()) }
+                .firstOrNull()
+        }
+        println(message)
+    }
 }
 
 suspend fun <K, V> Producer<K, V>.asyncSend(record: ProducerRecord<K, V>): RecordMetadata =
-  suspendCoroutine { continuation ->
-    send(record) { metadata, exception ->
-      exception?.let(continuation::resumeWithException)
-        ?: continuation.resume(metadata)
+    suspendCoroutine { continuation ->
+        send(record) { metadata, exception ->
+            exception?.let(continuation::resumeWithException)
+                ?: continuation.resume(metadata)
+        }
     }
-  }
 
 tailrec fun <T> repeatUntilSome(block: () -> T?): T = block() ?: repeatUntilSome(block)
